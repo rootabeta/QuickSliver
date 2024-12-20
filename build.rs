e217d6415f1a990098da0e79d121cfceee13fa66
rootabeta <rootabeta@protonmail.com>
2024-12-19 19:42:04 -0500

    Add build step to compile protobufs from Sliver repo

---
+++ build.rs
@@ -0,0 +1,28 @@
+// Stub to compile protobufs from Sliver for use elsewhere
+fn main() -> Result<(), Box<dyn std::error::Error>> { 
+    /* From the Sliver protobuf documentation:
+     * `commonpb` - Common generic messages shared between `clientpb` and `sliverpb`. 
+     *      -> Notably the generic `Request` and `Response` types, which are used as headers in gRPC request/responses.
+     * `clientpb` - These messages should _only_ be sent from the client to server or vice versa.
+     * `sliverpb` - These message may be sent from the client to the server or from the server to the implant and vice versa. 
+     *      -> Not all messages defined in this file will appear in client<->server communication, some are specific to implant<->server.
+     * `rpcpb` - gRPC service definitions
+     */
+
+    /*
+    tonic_build::compile_protos("sliver/protobuf/commonpb/common.proto")?;
+    tonic_build::compile_protos("sliver/protobuf/sliverpb/sliver.proto")?;
+    tonic_build::compile_protos("sliver/protobuf/clientpb/client.proto")?;
+    tonic_build::compile_protos("sliver/protobuf/rpcpb/services.proto")?;
+    */
+
+    tonic_build::configure()
+        .build_client(true) // We only care about the client tools
+        .build_server(false)
+        .compile_protos(
+            &["sliver/protobuf/rpcpb/services.proto"],
+            &["sliver/protobuf/"]
+        )?;
+
+    Ok(())
+}
